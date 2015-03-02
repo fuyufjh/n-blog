95b476ced878dcfc897c525df1c0de0ecf6678ec
Klose Fu <fuyufjh@163.com>
2015-03-03 00:32:58 +0800

    Custom 404 page

--- app.js
+++ app.js
@@ -55,9 +55,7 @@ app.use('/archive', routesArchive);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.render('404');
 });
 
 // error handlers
