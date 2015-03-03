1d0493bea317a2893e1180e20b41819d6e20fa6b
Klose Fu <fuyufjh@163.com>
2015-03-03 15:15:13 +0800

    New edit page

--- routes/edit.js
+++ routes/edit.js
@@ -7,11 +7,11 @@ router.get('/:name/:day/:title', checkLoginUser);
 router.get('/:name/:day/:title', function (req, res) {
   Post.getRaw(req.params.name, req.params.day, req.params.title, function (err, post) {
     if (err) {
-      req.flash('error', err);
+      req.flash('error', err.message);
       return res.redirect('/');
     }
     res.render('edit', {
-      title: post.title,
+      title: 'Edit',
       post: post,
       user: req.session.user,
       success: req.flash('success').toString(),
@@ -20,16 +20,20 @@ router.get('/:name/:day/:title', function (req, res) {
   });
 });
 
-router.post('/:name/:day/:title', checkLoginUser);
-router.post('/:name/:day/:title', function(req, res) {
-  Post.update(req.params.name, req.params.day, req.params.title, req.body.text, function(err) {
+router.post('/:id', checkLoginUser);
+router.post('/:id', function(req, res) {
+  var post_id = req.params.id,
+      tags = req.body.tags.split(/ *, */),
+      currentUser = req.session.user,
+      post = new Post(currentUser.name, req.body.title, req.body.text, tags);
+  post.update(post_id, currentUser.name, function(err) {
     if (err) {
-      req.flash('error', err);
-    } else {
-      req.flash('success', 'Article updated!');
+      req.flash('error', err.message);
+      return res.redirect('/');
     }
-    var url = encodeURI('/u/'+req.params.name+'/'+req.params.day+'/'+req.params.title);
-    res.redirect(url);
+    req.flash('success', 'Your article has been updated!');
+    var url = encodeURI('/u/' + req.body.name + '/' + req.body.date + '/' + req.body.title);
+    return res.redirect(url);
   });
 });
 
@@ -40,10 +44,12 @@ function checkLoginUser(req, res, next) {
     req.flash('error', 'Not login!');
     return res.redirect('/login');
   }
-  if (req.session.user.name != req.params.name) {
-    req.flash('error', 'You are not the author of this article.');
-    var url = encodeURI('/u/'+req.params.name+'/'+req.params.day+'/'+req.params.title);
-    return res.redirect(url);
+  if (req.params.name) { // for the GET method Only
+    if (req.session.user.name != req.params.name) {
+      req.flash('error', 'You are not the author of this article.');
+      var url = encodeURI('/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title);
+      return res.redirect(url);
+    }
   }
   next();
 }
